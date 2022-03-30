89929b6e85d86677961a80fcf638d82b5989d0f9
Igor <51010839+Sfrev@users.noreply.github.com>
2022-03-30 19:37:43 -0300

    Sound base and options slider

---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class PlayerPrefsController: MonoBehaviour
+{
+    private const string MasterVolumeKey = "master volume";
+    private const string MusicVolumeKey = "music volume";
+    private const string SoundEffectsVolumeKey = "sfx volume";
+
+    private const float MinVolume = 0f;
+    private const float MaxVolume = 1f;
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= MinVolume && volume <= MaxVolume)
+        {
+            PlayerPrefs.SetFloat(MasterVolumeKey, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume is out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MasterVolumeKey, 1f);
+    }
+    
+    public static void SetMusicVolume(float volume)
+    {
+        if (volume >= MinVolume && volume <= MaxVolume)
+        {
+            //Debug.Log($"Music volume set to { volume }");
+            PlayerPrefs.SetFloat(MusicVolumeKey, volume);
+        }
+        else
+        {
+            Debug.LogError("Music Volume is out of Range");
+        }
+    }
+
+    public static float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat(MusicVolumeKey, 1f);
+    }
+
+    public static void SetSoundEffectsVolume(float volume)
+    {
+        if (volume >= MinVolume && volume <= MaxVolume)
+        {
+            //Debug.Log($"Sound effects volume set to { volume }");
+            PlayerPrefs.SetFloat(SoundEffectsVolumeKey, volume);
+        }
+        else
+        {
+            Debug.LogError("Sound effects volume is out of Range");
+        }
+    }
+
+    public static float GetSoundEffectsVolume()
+    {
+        return PlayerPrefs.GetFloat(SoundEffectsVolumeKey, 1f);
+    }
+}
